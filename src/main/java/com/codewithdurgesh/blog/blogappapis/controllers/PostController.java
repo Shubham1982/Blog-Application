6fdfb0dbf720a29890a3b29340d7f95e54315b30
Shubham Gore <goreshubhamgore@gmail.com>
2024-07-07 12:11:43 +0530

    added postcontroller, Bug fixed in categore.java, changed password in the application.properties

--- src/main/java/com/codewithdurgesh/blog/blogappapis/controllers/PostController.java
+++ src/main/java/com/codewithdurgesh/blog/blogappapis/controllers/PostController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class PostController {
@@ -23,5 +25,17 @@ public class PostController {
     ){
         PostDto createPost = this.postService.createPost(postDto, userId, categoryId);
         return new ResponseEntity<PostDto>(createPost,HttpStatus.CREATED);
+    }
+
+    @GetMapping("/user/{userId}/posts")
+    public ResponseEntity<List<PostDto>> getPostByUser(@PathVariable Integer userId){
+        List<PostDto> posts = this.postService.getPostsByUser(userId);
+        return new ResponseEntity<>(posts,HttpStatus.OK);
+    }
+
+    @GetMapping("/category/{categoryId}/posts")
+    public ResponseEntity<List<PostDto>> getPostsByCategory(@PathVariable Integer categoryId){
+        List<PostDto> posts = this.postService.getPostsByCategory(categoryId);
+        return new ResponseEntity<>(posts,HttpStatus.OK);
     }
 }
