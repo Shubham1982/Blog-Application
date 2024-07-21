653cb1b06df5ace44c120adf4f42cbc253a669d7
Shubham Gore <goreshubhamgore@gmail.com>
2024-07-21 18:15:19 +0530

    comment implementation

--- src/main/java/com/codewithdurgesh/blog/blogappapis/payloads/PostDto.java
+++ src/main/java/com/codewithdurgesh/blog/blogappapis/payloads/PostDto.java
@@ -1,9 +1,12 @@
 package com.codewithdurgesh.blog.blogappapis.payloads;
 
+import com.codewithdurgesh.blog.blogappapis.entities.Comment;
 import com.codewithdurgesh.blog.blogappapis.entities.User;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 public class PostDto {
@@ -15,4 +18,5 @@ public class PostDto {
     private Date addedDate;
     private CategoryDto category;
     private UserDto user;
+    private Set<Comment> comments = new HashSet<>();
 }
